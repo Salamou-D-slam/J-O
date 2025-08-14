e46579922b97fef59bdf842fe753119aa400cc62
Islam <islamderrouiche@gmail.com>
2025-08-14 19:46:40 +0200

    amelioration pdf

--- app/services/ticket_pdf.py
+++ app/services/ticket_pdf.py
@@ -30,8 +30,23 @@ def generer_ticket_pdf(ticket):
     c.drawString(20 * mm, height - 80 * mm, f"Épreuve : {ticket.offre.epreuve.nom_epreuve}")
     c.drawString(20 * mm, height - 90 * mm, f"Date : {ticket.offre.epreuve.date_epreuve.strftime('%d/%m/%Y %H:%M')}")
     c.drawString(20 * mm, height - 100 * mm, f"Prix : {ticket.offre.prix} €")
-    c.drawString(20 * mm, height - 120 * mm, f"Personnes concernés : {ticket.pers_data}")
+   # c.drawString(20 * mm, height - 120 * mm, f"Personnes concernés : {ticket.pers_data}")
 
+
+    # Rendre ma table qui en jsonb en liste python
+    personnes = ticket.pers_data or []
+
+    c.setFont("Helvetica-Bold", 14)
+    c.drawString(20 * mm, height - 120 * mm, "Personnes concernées :")
+
+    y_position = height - 130 * mm
+    c.setFont("Helvetica", 12)
+
+    for p in personnes:
+        nom = p.get("nom", "")
+        prenom = p.get("prenom", "")
+        c.drawString(25 * mm, y_position, f"- {prenom} {nom}")
+        y_position -= 8 * mm
 
     # QR Code
     qr_path = os.path.join(current_app.static_folder, 'uploads', 'qrcodes', f"ticket_{ticket.id}.png") #vu que le qr code est déja crée dans la route du paiement, je dois juste le trouver grace aux chemin
--- app/ticket/routes.py
+++ app/ticket/routes.py
@@ -18,38 +18,6 @@ def ticket_detail(ticket_id):
     ticket = Ticket.query.get_or_404(ticket_id)
     return render_template('ticket_details.html', ticket=ticket)
 
-# @ticket_routes.route('/<int:ticket_id>/pdf', methods=['POST'])
-# @login_required
-# def ticket_pdf(ticket_id):
-#     ticket = Ticket.query.get_or_404(ticket_id)
-#
-#     qr_code = os.path.join(current_app.static_folder, 'uploads', 'qrcodes', ticket.qr_code)
-#
-#     # Rendu HTML du ticket
-#     html_content = render_template('ticket_details.html', ticket=ticket, qr_code=qr_code)
-#
-#     # Générer le PDF avec base_url pour charger les fichiers locaux
-#     #pdf = HTML(string=html_content, base_url=current_app.root_path).write_pdf()
-#
-#     # Générer le PDF depuis le HTML
-#     pdf = HTML(string=html_content).write_pdf()
-#
-#     # Définir le chemin de sauvegarde
-#     pdf_folder = os.path.join(current_app.static_folder, 'uploads', 'tickets')
-#     os.makedirs(pdf_folder, exist_ok=True)  # Crée le dossier si inexistant
-#     pdf_filename = f'ticket_{ticket.id}.pdf'
-#     pdf_path = os.path.join(pdf_folder, pdf_filename)
-#
-#     # # Sauvegarder le PDF
-#     with open(pdf_path, 'wb') as f:
-#         f.write(pdf)
-#
-#
-#     #Retourner le PDF en téléchargement
-#     response = make_response(pdf)
-#     response.headers['Content-Type'] = 'application/pdf'
-#     response.headers['Content-Disposition'] = f'attachment; filename=ticket_{ticket.id}.pdf'
-#     return response
 
 @ticket_routes.route('/<int:ticket_id>/pdf', methods=['POST'])
 @login_required
@@ -58,6 +26,6 @@ def ticket_pdf(ticket_id):
     pdf_path = generer_ticket_pdf(ticket)
     return send_file(os.path.join("static", pdf_path),
                      as_attachment=True,
-                     download_name=f"ticket_{ticket.id}.pdf")
+                     download_name=f"ticket_{ticket.user.nom}_{ticket.offre.type_offre}_{ticket.id}.pdf")
 
 
