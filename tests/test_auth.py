92d4609d3421a28a6d5c981ee501d93ffeba3ee1
Islam <islamderrouiche@gmail.com>
2025-09-07 20:55:10 +0200

    debut test ticket et paiement

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,34 +1,27 @@
 import pytest
-
-import sys
-import os
 from werkzeug.security import generate_password_hash
-
-
-# Ajouter le dossier racine au PYTHONPATH pour que Python trouve app/
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
+from configtest import client
 from app import create_app, db
 from app.models import User
 
-@pytest.fixture
-def client():
-    test_config = {
-        "TESTING": True,
-        "SQLALCHEMY_DATABASE_URI": 'postgresql+psycopg2://jo_admin:joadmin2024@localhost:5432/jo_test',
-        "WTF_CSRF_ENABLED": False,
-    }
-
-    app = create_app(test_config)  # On passe la config test
-
-
-    with app.test_client() as client:
-            with app.app_context():
-                db.create_all()
-            yield client
-            # Nettoyage après chaque test
-            with app.app_context():
-                db.drop_all()
+# @pytest.fixture
+# def client():
+#     test_config = {
+#         "TESTING": True,
+#         "SQLALCHEMY_DATABASE_URI": 'postgresql+psycopg2://jo_admin:joadmin2024@localhost:5432/jo_test',
+#         "WTF_CSRF_ENABLED": False,
+#     }
+#
+#     app = create_app(test_config)  # On passe la config test
+#
+#
+#     with app.test_client() as client:
+#             with app.app_context():
+#                 db.create_all()
+#             yield client
+#             # Nettoyage après chaque test
+#             with app.app_context():
+#                 db.drop_all()
 
 
 def test_register(client):
